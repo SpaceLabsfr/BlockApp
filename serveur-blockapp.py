843e6f6ee4cab5c7d778322e5862c3b4244fbc2e
Kevin Desir <76001870+Kevin-Desir@users.noreply.github.com>
2022-04-21 15:31:32 +0200

    Update serveur-blockapp.py

--- serveur-blockapp.py
+++ serveur-blockapp.py
@@ -3,22 +3,49 @@ import time
 import sys
 from multiprocessing import Process, Value
 import zmq
+import Jetson.GPIO as GPIO
+
+pinrun = 'DAP4_SCLK' #12
+pinbouton = 'SPI2_SCK' #13
+pinau = 'SPI2_CS1' #16
+autrepin = 'SPI2_CS0' #18 
+
+GPIO.setmode(GPIO.TEGRA_SOC)
+GPIO.setup(pinrun, GPIO.OUT)
+GPIO.output(pinrun, 0)
+
+GPIO.setup(pinau, GPIO.OUT)
+GPIO.output(pinau, 0)
+
+GPIO.setup(pinbouton, GPIO.IN)
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-
 currentlyRunning = Value('b', False)
 
 def RunScript(script, data):
 	#print("running " + script)
+	GPIO.output('DAP4_SCLK', 1)
 	data.value = True
 	exec(script)
 	print(f"currentlyRunning from process {currentlyRunning.value}")
 	print("Script terminé avec succès")
 	data.value = False
 	print(f"currentlyRunning from process {currentlyRunning.value}")
+	GPIO.output('DAP4_SCLK', 0)
+
+def BumperChock(data):
+	print("Detection pare-chocs")
+	if currentlyRunning.value:
+		runThread.terminate() # sends a SIGTERM
+		currentlyRunning.value = False;
+		car.throttle = 0.001
+		car.throttle = 0
+		GPIO.output(pinrun, 0)
+		GPIO.output(pinau, 1)
+		data.value = False
 
 car = NvidiaRacecar()
 car.steering_gain = -0.65
@@ -27,15 +54,25 @@ if car.steering_offset != -0.25 : exit()
 
 print("Car ready")
 
+GPIO.add_event_detect(pinbouton, GPIO.FALLING, callback=lambda x: BumperChock(currentlyRunning), bouncetime=10)
+		
 while True:
 	try:
 		print("en attente recv...")
 		message = socket.recv()
+		GPIO.output(pinau, 0)
 		socket.send(b"OK")
 		message = message.decode("utf-8")
-		#print("Received request: %s" % message)
 		
-		print(message)
+		#print("Received request: %s" % message)
+		f = open("/KDesir_Tests/logging.txt", "a")
+
+		t = time.strftime('%d/%m/%Y-%H:%M:%S', time.localtime()) + ","
+		log = message.replace("\n", "\n" + t)
+		f.write(t + log + "\n")
+		f.close()
+		
+		#print(message)
 
 		if "ArretUrgence" in message:
 			runThread.terminate() # sends a SIGTERM
@@ -54,7 +91,13 @@ while True:
 
 	except Exception as e:
 		print(e)
+
 		car.throttle = 0.001
 		car.throttle = 0
+		GPIO.output(pinrun, 0)
+		GPIO.output(pinau, 1)
+
+	#finally:
+#		GPIO.cleanup()
 
 sys.exit("Fin du programme")
